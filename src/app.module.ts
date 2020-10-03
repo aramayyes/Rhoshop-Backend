62426e19e8ec6f9a6d6a8fa443c498af337fe464
aramayyes <aramayis.amiraghyan@outlook.com>
2020-10-03 22:43:55 +0400

    Add category images.
    Signed-off-by: aramayyes <aramayis.amiraghyan@outlook.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+    }),
     MongooseModule.forRoot('mongodb://localhost:27017/roshop'),
     CategoriesModule,
   ],
