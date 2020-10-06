1d6cf1c66bbcc313333f902cf90b4075117c622c
aramayyes <aramayis.amiraghyan@outlook.com>
2020-10-06 10:16:47 +0400

    Add create user query.
    Signed-off-by: aramayyes <aramayis.amiraghyan@outlook.com>

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,15 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateUserDto, UserDto } from './dto';
+import { UsersService } from './users.service';
 
 @Resolver()
-export class UsersResolver {}
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Mutation('createUser')
+  async create(
+    @Args('createUserInput') userInput: CreateUserDto,
+  ): Promise<UserDto> {
+    return this.usersService.create(userInput);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas';
+import { CreateUserDto, UserDto } from './dto';
+import { createHash } from '../utils';
+import { ProductDto } from '../products/dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly usersModel: Model<User>,
+  ) {}
+
+  /**
+   * Finds a user with given email.
+   * @param email Email of wanted user.
+   */
+  async findByEmail(email: string): Promise<UserDto> {
+    const users: UserDto[] = await this.usersModel
+      .aggregate<ProductDto>([
+        {
+          $match: {
+            email: email,
+          },
+        },
+        {
+          $project: {
+            id: '$_id',
+            _id: 0,
+            name: 1,
+            phoneNumber: 1,
+            email: 1,
+          },
+        },
+      ])
+      .exec();
+
+    if (users.length !== 0) {
+      return users[0];
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Creates a user.
+   * @param userPayload
+   */
+  async create(userPayload: CreateUserDto): Promise<UserDto> {
+    userPayload.password = await createHash(userPayload.password);
+    return new UserDto(await this.usersModel.create(userPayload));
+  }
+}
