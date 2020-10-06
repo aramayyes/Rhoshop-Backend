38c499c6c7db84bda0f3c414a46e0249533c6c3d
aramayyes <aramayis.amiraghyan@outlook.com>
2020-10-06 09:41:20 +0400

    Add crypto util.
    Signed-off-by: aramayyes <aramayis.amiraghyan@outlook.com>

---
+++ src/utils/crypto.util.ts
@@ -0,0 +1,25 @@
+import * as bCrypt from 'bcrypt';
+
+/**
+ * @function
+ * @description Creates hash of given password.
+ * @param password
+ * @return Hashed password.
+ */
+export const createHash = async (password: string) => {
+  return bCrypt.hash(password, 10, null);
+};
+
+/**
+ * @function
+ * @description Compares the given password with the hash password.
+ * @param password
+ * @param hashPassword
+ * @return True if passwords are equal, false otherwise.
+ */
+export const isValidPassword = async (
+  password: string,
+  hashPassword: string,
+): Promise<boolean> => {
+  return bCrypt.compare(password, hashPassword);
+};
