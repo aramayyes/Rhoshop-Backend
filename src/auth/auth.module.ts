ffc993aa4f093443ba42cdec52419428b3ab331e
aramayyes <aramayis.amiraghyan@outlook.com>
2020-10-06 17:57:10 +0400

    Add CurrentUser decorator.
    Signed-off-by: aramayyes <aramayis.amiraghyan@outlook.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthResolver } from './auth.resolver';
 import { AppConfigService } from '../app-config/app-config.service';
+import { JwtStrategy } from './strategies';
 
 @Module({
   imports: [
@@ -16,6 +17,6 @@ import { AppConfigService } from '../app-config/app-config.service';
     }),
     UsersModule,
   ],
-  providers: [AuthService, AuthResolver],
+  providers: [AuthService, AuthResolver, JwtStrategy],
 })
 export class AuthModule {}
