1834874b27f02c5521dac685d569b03a5ccbe40a
aramayyes <aramayis.amiraghyan@outlook.com>
2020-10-07 17:27:10 +0400

    Add orders query and createOrder mutation.
    Signed-off-by: aramayyes <aramayis.amiraghyan@outlook.com>

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,4 +1,58 @@
-import { Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto, OrderDto } from './dto';
+import { GqlJwtAuthGuard } from '../auth/guards';
+import { UseGuards } from '@nestjs/common';
+import { CurrentUser } from '../current-user.decorator';
+import { ProductsService } from '../products/products.service';
+import { UserInputError } from 'apollo-server-express';
+import { ProductArgs } from './args';
+import { ProductDto } from '../products/dto';
+import { ProductLoaders } from '../products/product.loaders';
 
-@Resolver()
-export class OrdersResolver {}
+@Resolver('Order')
+export class OrdersResolver {
+  constructor(
+    private readonly ordersService: OrdersService,
+    private readonly productsService: ProductsService,
+    private readonly productLoaders: ProductLoaders,
+  ) {}
+
+  @Mutation('createOrder')
+  @UseGuards(GqlJwtAuthGuard)
+  async createOrder(
+    @Args('createOrderInput') orderInput: CreateOrderDto,
+    @CurrentUser() user: { id: string; email: string },
+  ): Promise<OrderDto> {
+    if (!(await this.productsService.exists(orderInput.product))) {
+      throw new UserInputError('Invalid product!');
+    }
+    return this.ordersService.create(user.id, orderInput);
+  }
+
+  @Query('orders')
+  @UseGuards(GqlJwtAuthGuard)
+  async getOrders(
+    @CurrentUser() user: { id: string; email: string },
+  ): Promise<OrderDto[]> {
+    return this.ordersService.findMany(user.id);
+  }
+
+  @ResolveField('product')
+  async product(
+    @Parent() order,
+    @Args() args: ProductArgs,
+  ): Promise<ProductDto> {
+    return this.productLoaders.findByIds.load({
+      id: order.product.toString(),
+      language: args.language,
+    });
+  }
+}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Order } from './schemas';
+import { CreateOrderDto, OrderDto } from './dto';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(
+    @InjectModel(Order.name) private readonly orderModel: Model<Order>,
+  ) {}
+
+  /**
+   * Finds orders of given user.
+   * @param user Id of the user whose orders should be find.
+   */
+  async findMany(user: string): Promise<OrderDto[]> {
+    return await this.orderModel
+      .aggregate<OrderDto>([
+        {
+          $match: {
+            user: new Types.ObjectId(user),
+          },
+        },
+        {
+          $sort: {
+            date: -1,
+          },
+        },
+        {
+          $project: {
+            id: '$_id',
+            _id: 0,
+            date: 1,
+            product: 1,
+            productSize: 1,
+            productColor: 1,
+            productCount: 1,
+          },
+        },
+      ])
+      .exec();
+  }
+
+  /**
+   * Creates an order.
+   * @param user Id of the user who the order should be created for.
+   * @param orderPayload Contains order details.
+   */
+  async create(user: string, orderPayload: CreateOrderDto): Promise<OrderDto> {
+    return new OrderDto(
+      await this.orderModel.create({ user, date: new Date(), ...orderPayload }),
+    );
+  }
+}
