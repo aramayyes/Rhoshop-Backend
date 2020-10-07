5b27c0b25db810bd50cb8710bbe2625cdb1f05c8
aramayyes <aramayis.amiraghyan@outlook.com>
2020-10-07 10:52:18 +0400

    Add categories seeding.
    Signed-off-by: aramayyes <aramayis.amiraghyan@outlook.com>

---
+++ src/seeding/categories.ts
@@ -0,0 +1,46 @@
+export default [
+  [
+    {
+      _id: '5f78c9e644a3aa066cf347c6',
+      order: 10,
+      name: 'Tops',
+      nameRu: 'Топы',
+      image: 'images/categories/tops.png',
+    },
+    {
+      _id: '5f78c9e644a3aa066cf347c7',
+      order: 20,
+      name: 'Underwear',
+      nameRu: 'Белье',
+      image: 'images/categories/underwear.png',
+    },
+    {
+      _id: '5f78c9e644a3aa066cf347c8',
+      order: 30,
+      name: 'Sweaters',
+      nameRu: 'Джемперы',
+      image: 'images/categories/sweaters.png',
+    },
+    {
+      _id: '5f78c9e644a3aa066cf347c9',
+      order: 40,
+      name: 'Dresses',
+      nameRu: 'Платья',
+      image: 'images/categories/dresses.png',
+    },
+    {
+      _id: '5f78c9e644a3aa066cf347ca',
+      order: 50,
+      name: 'Jeans',
+      nameRu: 'Джинсы',
+      image: 'images/categories/jeans.png',
+    },
+    {
+      _id: '5f78c9e644a3aa066cf347cb',
+      order: 60,
+      name: 'Shorts',
+      nameRu: 'Шорты',
+      image: 'images/categories/shorts.png',
+    },
+  ],
+];
--- src/seeding/index.ts
+++ src/seeding/index.ts
@@ -1,20 +1,56 @@
 import * as mongoose from 'mongoose';
 import { NotificationSchema } from '../notifications/schemas';
+import { CategorySchema } from '../categories/shemas';
 import notifications from './notifications';
+import categories from './categories';
 
 const NotificationsModel = mongoose.model('Notifications', NotificationSchema);
+const CategoriesModel = mongoose.model('Categories', CategorySchema);
 
-mongoose
-  .connect('mongodb://localhost:27017/roshop', {
+async function connect() {
+  await mongoose.connect('mongodb://localhost:27017/roshop', {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Adding notifications');
-    return Promise.all(notifications.map(n => NotificationsModel.create(n)));
-  })
-  .then(() => console.log('Notifications added.'))
+  });
+}
+
+async function seed() {
+  await connect();
+
+  const categoryDocs = await seedCollection(
+    CategoriesModel,
+    'Categories',
+    categories,
+  );
+
+  const notificationDocs = await seedCollection(
+    NotificationsModel,
+    'Notifications',
+    notifications,
+  );
+}
+
+async function seedCollection(
+  model: mongoose.Model<mongoose.Document>,
+  collectionName: string,
+  collection: any[],
+) {
+  let items = await model.find({});
+
+  if (items.length != 0) {
+    console.log(`${collectionName} are already inserted. Skipping...`);
+    return items;
+  }
+
+  console.log(`Inserting ${collectionName}...`);
+  items = await Promise.all(collection.map(c => CategoriesModel.create(c)));
+  console.log(`${collectionName} are inserted.`);
+
+  return items;
+}
+
+seed()
   .catch(e => console.log(e))
   .finally(() => process.exit());
